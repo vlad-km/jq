3570a5df78084803de17b7f0d7abd57ade4cf56c
vlad-km <gm5461@gmail.com>
2024-02-27 22:08:02 +0300

    Update jq-dom-inside.lisp

--- dom/jq-dom-inside.lisp
+++ dom/jq-dom-inside.lisp
@@ -18,86 +18,66 @@
 
 (in-package :jq)
 
-;;;
+
 ;;; These methods allow us to insert new content inside an existing element.
-;;;
 ;;; See: http://api.jquery.com/category/manipulation/dom-insertion-inside/
-;;;
 
 
 ;;; .append()
 ;;; Insert content, specified by the parameter, to the end of each element in the set of matched elements.
-
-(defun append (jse content)
-    (funcall ((oget jse "append" "bind") jse content)))
-
 (export '(jq::append))
-
+(defun append (jse content) (@call (jse "append") content))
 
 ;;; .appendTo()
 ;;; Insert every element in the set of matched elements to the end of the target.
-(defun append-to (jse content)
-    (funcall ((oget jse "appendTo" "bind") jse content)))
-
 (export '(jq::append-to))
-
-
+(defun append-to (jse content) (@call (jse "appendTo") content))
 
 ;;; jq:html
-;;;
 ;;; (jq:html jqe)
 ;;; =>
 ;;;      Get the HTML contents of the first element in the set of matched elements
-;;;
 ;;; (jq:html jqe "<p>All new content. <em>You bet!</em></p>")
-;;;
-;;;      midagi string or function
-;;;      A string of HTML to set as the content of each matched element.
-;;;      A function returning the HTML content to set.
-;;;              => (fn index old-html)
-;;;              <= html-string
-;;;
-
-(defun html (jse &optional midagi)
-    (if midagi
-        (funcall ((oget jse "html" "bind") jse midagi))
-        (funcall ((oget jse "html" "bind") jse))))
-
+;;;      setter - string or function
+;;;               A string of HTML to set as the content of each matched element.
+;;;               A function returning the HTML content to set.
+;;;                => (fn index old-html)
+;;;                <= html-string
 (export '(jq::html))
-
+(defun html (jse &optional (setter "")) (@call (jse "appendTo") setter))
 
 ;;; .prepend()
 ;;; Insert content, specified by the parameter, to the beginning of each element in the set of matched elements.
-
-(defun prepend (jse content &optional midagi)
-    (if midagi
-        (funcall ((oget jse "prepend" "bind") jse content midagi))
-        (funcall ((oget jse "prepend" "bind") jse content))))
-
+;;; content:  htmlString or Element or Text or Array or jQuery
+;;;          DOM element, text node, array of elements and text nodes,
+;;;          HTML string, or jQuery object to insert at the beginning of each
+;;;          element in the set of matched elements.
+;;; content1: htmlString or Element or Text or Array or jQuery
+;;;           One or more additional DOM elements, text nodes, arrays of elements
+;;;           and text nodes, HTML strings, or jQuery objects to insert at the beginning
+;;;           of each element in the set of matched elements.
 (export '(jq::prepend))
-
-
-
+(defun prepend (jse content &optional (midagi "")) (@call (jse "appendTo") content midagi))
 
 ;;; .prependTo()
 ;;; Insert every element in the set of matched elements to the beginning of the target.
-(defun prepend-to (jse target)
-    (funcall ((oget jse "prependTo" "bind") jse target)))
-
+;;; target  Selector or htmlString or Element or Array or jQuery
+;;;         A selector, element, HTML string, array of elements, or jQuery object
+;;;         the matched set of elements will be inserted at the beginning of the
+;;;         element(s) specified by this parameter.
 (export '(jq::prepend-to))
-
+(defun prepend-to (jse target) (@call (jse "prependTo") target))
 
 ;;; .text()
 ;;; Get the combined text contents of each element in the set of matched elements, including their
 ;;; descendants, or set the text contents of the matched elements.
-;;;
-;;;
-(defun text (jse &optional midagi)
-    (if midagi
-        (funcall ((oget jse "text" "bind") jse midagi))
-        (funcall ((oget jse "text" "bind") jse))))
-
+;;; content - String or Number or Boolean
+;;;           The text to set as the content of each matched element.
+;;;           When Number or Boolean is supplied, it will be converted
+;;;           to a String representation.
 (export '(jq::text))
+(defun text (jse &optional (content "")) (@call (jse "text") target))
+
 
 (in-package :cl-user)
 
