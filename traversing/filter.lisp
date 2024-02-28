67371dc109353772481867d95e447e36770f646e
vlad-km <gm5461@gmail.com>
2024-02-28 12:31:33 +0300

    Create filter.lisp

---
+++ traversing/filter.lisp
@@ -0,0 +1,65 @@
+;;; -*- mode:lisp;  coding:utf-8 -*-
+#|
+
+            /\___/\
+            )     (
+           =\     /=                  if this code is not work, i dont know who wrote this code
+             )   (                    Copyright © 2017,2018,2022  @vlad-km
+            /     \                   2017, Original https://github.com/vlad-km/moren-electron
+            )     (                   2022, Code redesign
+           /       \                  Electron >= electron@21.2.2
+           \       /                  JSCL >= version 0.8.2  
+      jgs   \__ __/
+               ))
+              //
+             ((
+              \)
+|#
+
+;;; Category: Traversing Filterring
+;;; https://api.jquery.com/category/traversing/
+
+
+;;; .eq()
+;;; Reduce the set of matched elements to the one at the specified index.
+
+;;; .even()
+;;; Reduce the set of matched elements to the even ones in the set, numbered from zero.
+
+;;; .filter()
+;;; Reduce the set of matched elements to those that match the selector or pass the function’s test.
+
+;;; .find()
+;;; Get the descendants of each element in the current set of matched elements, filtered by a selector, 
+;;; jQuery object, or element.
+
+;;; .first()
+;;; Reduce the set of matched elements to the first in the set.
+
+;;; .has()
+;;; Reduce the set of matched elements to those that have a descendant that matches the selector or DOM element.
+
+;;; .is()
+;;; Check the current matched set of elements against a selector, element, or jQuery object and return true 
+;;; if at least one of these elements matches the given arguments.
+
+;;; .last()
+;;; Reduce the set of matched elements to the final one in the set.
+
+;;; .map()
+;;; Pass each element in the current matched set through a function, producing a new jQuery object containing the return values.
+
+;;; .not()
+;;; Remove elements from the set of matched elements.
+
+;;; .odd()
+;;; Reduce the set of matched elements to the odd ones in the set, numbered from zero.
+
+;;; .slice()
+;;; Reduce the set of match
+
+
+
+
+
+;;; EOF
