290cf463cb593d6427cbe1f8e45c607363da7269
vlad-km <gm5461@gmail.com>
2024-02-28 12:31:37 +0300

    Create misc.lisp

---
+++ traversing/misc.lisp
@@ -0,0 +1,39 @@
+;;; -*- mode:lisp;  coding:utf-8 -*-
+#|
+
+            /\___/\
+            )     (
+           =\     /=                  if this code is not work, i dont know who wrote this code
+             )   (                    Copyright © 2017,2018,2022  @vlad-km
+            /     \                   2017, Original https://github.com/vlad-km/moren-electron
+            )     (                   2022, Code redesign
+           /       \                  Electron >= electron@21.2.2
+           \       /                  JSCL >= version 0.8.2  
+      jgs   \__ __/
+               ))
+              //
+             ((
+              \)
+|#
+
+Category: Traversing Miscellaneous Traversing
+https://api.jquery.com/category/traversing/
+
+
+;;; .add()
+;;; Create a new jQuery object with elements added to the set of matched elements.
+
+;;; .addBack()
+;;; Add the previous set of elements on the stack to the current set, optionally filtered by a selector.
+
+
+;;; .contents()
+;;; Get the children of each element in the set of matched elements, including text and comment nodes.
+
+;;; .each()
+;;; Iterate over a jQuery object, executing a function for each matched element.
+
+;;; .end()
+;;; End the most recent filtering operation in the current chain and return the set of matched elements to its previous state.
+
+;;; EOF
